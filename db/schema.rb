49493e6dd31fdd6b7496e8da8744091ffa223adf
Martin Burke <mahtyb@gmail.com>
2013-05-31 16:42:22 -0700

    Add db/seeds.  Fix css for forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130531183013) do
+ActiveRecord::Schema.define(:version => 20130531225848) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
