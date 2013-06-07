3cc4173f683901fe79c81acc4a41f78e3abb036c
Martin Burke <mahtyb@gmail.com>
2013-06-07 07:01:30 -0700

    Cosmetic touches

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,7 +14,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
-    @project = current_user.projects.find(params[:id])
+    @project = Project.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
